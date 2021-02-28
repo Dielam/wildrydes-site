ab74b01904f56c8e2fcd8ba2afb6b3a54743bc9a
User1 <User1@574742832269>
2021-02-28 18:36:33 +0100

    WebApplication
    La aplicación realizada en el taller de AWS

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_OC6b8m9V9', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '63nkb7pp5a06lhsgb4juc5pq57', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: ' https://8i55e6qt2h.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
